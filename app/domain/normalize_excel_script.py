b16fe000628e12523c034aefd566c84fad63e66e
Julio <velascojuliocv@gmail.com>
2026-02-15 04:02:48 +0100

    feature: script

---
+++ app/domain/normalize_excel_script.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import pandas as pd
+import re
+
+
+class NormalizeExcelScript:
+    def __init__(self, excel_path: Path, excel_output_path: Path) -> None:
+        self._excel_path: Path = excel_path
+        self._excel_output_path: Path = excel_output_path
+
+    def run_script(self) -> None:
+        df = self._load_excel()
+        df = self._parse_types(df)
+        df = self._normalize_gender(df)
+        df = self._clean_nationalities(df)
+        df = self._capitalize_nationalities(df)
+        df = self._apply_nationality_exceptions(df)
+        df = self._build_final_nationality(df)
+        self._export_excel(df)
+
+    def _parse_types(self, df: pd.DataFrame) -> pd.DataFrame:
+        result = df.copy()
+        result["Fecha de Arresto"] = pd.to_datetime(
+            result["Fecha de Arresto"],
+            format="%d/%m/%Y %I:%M%p",
+            errors="coerce",
+        )
+        return result
+
+    def _normalize_gender(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _translate_gender(a_value: str) -> str | None:
+            GENDER_TRANSLATIONS = {
+                "male": "Masculino",
+                "female": "Femenino",
+            }
+
+            if not a_value:
+                return a_value
+
+            return GENDER_TRANSLATIONS.get(a_value.lower(), a_value)
+
+        result = df.copy()
+        result["Género"] = result["Género"].apply(_translate_gender)
+        return result
+
+    def _load_excel(self):
+        result: pd.DataFrame = (
+            pd.read_excel(self._excel_path, engine="calamine", dtype=object)
+            .fillna("")
+            .astype(str)
+        )
+        result.insert(0, "#", range(1, len(result) + 1))
+        return result
+
+    def _clean_nationalities(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _clean_value(value: str) -> str | None:
+            if not value:
+                return None
+
+            cleaned = re.sub(r"\s*\([^)]*\)", "", value).strip()
+
+            return cleaned if cleaned else None
+
+        result = df.copy()
+        for column in ["Nationality", "Segunda Nacionalidad"]:
+            result[column] = result[column].apply(_clean_value)
+
+        return result
+
+    def _apply_nationality_exceptions(self, df: pd.DataFrame) -> pd.DataFrame:
+        NATIONALITY_EXCEPTIONS: dict[str, str] = {
+            "trinidad y tobago": "Trinidad y Tobago",
+            "portugees": "Portugues",
+        }
+
+        def _apply_exceptions(value: str | None) -> str | None:
+            if not isinstance(value, str) or not value:
+                return None
+            return NATIONALITY_EXCEPTIONS.get(value.lower().strip(), value)
+
+        result = df.copy()
+        result["Segunda Nacionalidad"] = result["Segunda Nacionalidad"].apply(
+            _apply_exceptions
+        )
+        return result
+
+    def _capitalize_nationalities(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _capitalize(value: str | None) -> str | None:
+            if not isinstance(value, str) or not value:
+                return None
+            return value.title()
+
+        result = df.copy()
+        for column in ["Nationality", "Segunda Nacionalidad"]:
+            result[column] = result[column].apply(_capitalize)
+        return result
+
+    def _build_final_nationality(self, df: pd.DataFrame) -> pd.DataFrame:
+        def _combine(row: pd.Series) -> str | None:
+            nat1 = row.get("Nationality")
+            nat2 = row.get("Segunda Nacionalidad")
+
+            has_nat1 = isinstance(nat1, str) and nat1
+            has_nat2 = isinstance(nat2, str) and nat2
+
+            if has_nat1 and has_nat2:
+                return f"{nat2} / {nat1}"
+
+            if has_nat1:
+                return nat1
+
+            if has_nat2:
+                return nat2
+
+            return None
+
+        result = df.copy()
+        result["Nationality"] = result.apply(_combine, axis=1)  # type: ignore
+        return result
+
+    def _export_excel(self, df: pd.DataFrame) -> None:
+        with pd.ExcelWriter(
+            self._excel_output_path, engine="xlsxwriter", datetime_format="dd/mm/yyyy"
+        ) as writer:
+            df.to_excel(writer, index=False, sheet_name="Sheet1")
--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+from pathlib import Path
+from app.domain.normalize_excel_script import NormalizeExcelScript
+import sys
+
+
 def main():
-    print("Hello from asdf!")
+    input_path: Path = Path(
+        "/home/jvelasco/Desktop/mami/data/foropenal.detained-people-202602061640-(2).xlsx"
+    )
+    if not input_path.exists:
+        raise FileNotFoundError
+
+    output_path: Path = Path("Hello.xlsx")
+    if output_path.exists():
+        resonse: int = int(
+            input(f"Archivo f{output_path} ya Existe. Desea sobreescribir?")
+        )
+        match resonse:
+            case 1:
+                NormalizeExcelScript(input_path, output_path).run_script()
+            case 2:
+                sys.exit()
+            case _:
+                NormalizeExcelScript(input_path, output_path).run_script()
 
 
 if __name__ == "__main__":
